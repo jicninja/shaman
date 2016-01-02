9b7a72f65103ffa62882a17d835cbdec31c06bf8
Ignacio Castro <ignacio@venzee.com>
2016-01-02 15:59:40 -0300

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -35,14 +35,14 @@ gulp.task('copyDev', function () {
 
 
 gulp.task('uglify:vendor', function() {
-    return gulp.src(['./public/app/pixi.js','./public/app/socketio.js'])
+    return gulp.src(['./public/app/pixi.js','./public/app/socketio.js','./public/app/lodash.js'])
         .pipe(concat('vendor.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./dist/app'))
 });
 
 gulp.task('uglify:app', function() {
-   return gulp.src(['!./public/app/pixi.js','!./public/app/socketio.js','./public/app/*.js'])
+   return gulp.src(['!./public/app/pixi.js','!./public/app/socketio.js','!./public/app/lodash.js','./public/app/*.js'])
         .pipe(concat('app.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./dist/app'))
