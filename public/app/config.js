6acaab57a2ddcfc7013e19e3c2e4eb6786cc7cba
Ignacio Castro <ignacio@venzee.com>
2016-01-03 01:42:44 -0300

    init config module

---
+++ public/app/config.js
@@ -0,0 +1,54 @@
+var CGJ = {
+
+    fps: 60,
+
+    gravity: {
+        max: 40,
+        acceleration: 1
+    },
+
+
+    players : {
+
+        //vidas
+        lives: 3,
+
+        //posición inicial
+        default_position: {
+            floor: 180,
+            left: 30
+        },
+
+        //velocidades de correr
+        velocity: {
+            max: 15,
+            min: 3
+        },
+
+        //transparencias por tipos de jugador
+        alpha: {
+            PLAYABLE: 1,
+            ENEMY: 0.5
+        },
+
+        //fuerza de salto
+        jump: 17,
+
+        //aceleracion de pique
+        acceleration: {
+            increase: 0.1,
+            inertia: 0.5
+        },
+
+        //tipo de jugadores
+        type: {
+            PLAYABLE: 'CGJ.playable',
+            ENEMY:    'CGJ.enemy'
+        },
+
+        //id
+        default_id : 'self'
+    }
+
+
+};
